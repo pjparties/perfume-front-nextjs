793edc8c22a50d91c77755d51976f9356efe1464
parth <parthhjuneja@gmail.com>
2024-07-04 21:56:41 +0530

    add: random button function

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 'use client'
 
-import { useState } from 'react'
 import { Button } from "@/components/ui/button"
 import { SearchBar } from "@/components/SearchBar"
-import { useRouter } from 'next/navigation'
 import { ArrowRight } from 'lucide-react'
 import Link from 'next/link'
 
@@ -16,18 +14,23 @@ const displayScents = [
 
 
 export default function Home() {
-  const router = useRouter()
+  const findRandom = () => {
+    const randomRange = 1000
+    const randomKey = Math.floor(Math.random() * randomRange)
+    return randomKey;
+  }
 
   return (
     <div className="flex flex-col min-h-screen">
       <main className="flex-1 bg-background py-12 px-6">
         <div className="max-w-2xl mx-auto flex flex-col items-center gap-6">
           <h1 className="text-4xl font-bold text-center">Find Your Signature Scent</h1>
-          <p className="text-muted-foreground text-lg text-center">Search and select perfumes to get recommendations</p>
+          <p className="text-muted-foreground text-lg text-center">Search for your favorite perfumes to find alternatives and fragrances that will give the same vibes.</p>
           <div className="w-full flex flex-col gap-4">
             <div className="flex items-center gap-4">
               <SearchBar />
             </div>
+            {/* suggested perfumes */}
             <div className="flex flex-wrap gap-2 justify-center">
               {displayScents.map((scent) => (
                 <Link key={scent.key} href={`/recommendation/${encodeURIComponent(scent.key.toString(),)}`}>
@@ -41,12 +44,16 @@ export default function Home() {
                   </Button>
                 </Link>
               ))}
-              <Button
-                className="rounded-full bg-secondary text-secondary-foreground px-4 py-2 hover:bg-secondary-90 focus:outline-none focus:ring-1 focus:ring-secondary focus:border-secondary flex items-center gap-2"
-              >
-                Find Random
-                <ArrowRight className="h-4 w-4" />
-              </Button>
+              {/* random button */}
+              <Link href={`/recommendation/${findRandom()}`}>
+                <Button
+                  variant="ghost"
+                  className="rounded-full bg-muted text-muted-foreground px-4 py-2 hover:bg-muted-90 focus:outline-none focus:ring-1 focus:ring-primary focus:border-primary flex items-center gap-2"
+                >
+                  Suggest Me A Random Perfume
+                  <ArrowRight className="h-4 w-4" />
+                </Button>
+              </Link>
             </div>
           </div>
         </div>
